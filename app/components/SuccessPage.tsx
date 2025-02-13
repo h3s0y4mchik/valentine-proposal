f98552be19fb16374b0d512ce45effc7184b0c2e
Nikita Chernysh <h3s0y4mchik@gmail.com>
2025-02-13 22:48:38 +0100

    cat added 2

--- app/components/SuccessPage.tsx
+++ app/components/SuccessPage.tsx
@@ -5,7 +5,7 @@ export default function SuccessPage() {
     <div className="text-center">
       <h2 className="mb-8 text-3xl font-bold text-pink-600">Yay! You said yes!</h2>
       <p className="mb-4 text-xl text-pink-600">Now return to telegram (:</p>
-      <Image src="/placeholder.svg?height=200&width=200" alt="QR Code" width={200} height={200} className="mx-auto" />
+      <Image src="/cat.png?height=200&width=200" alt="QR Code" width={200} height={200} className="mx-auto" />
     </div>
   )
 }
